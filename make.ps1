ef3d20a0c819a1630fbfee39177514727cabe99f
Brian Scholer <brian@briantist.com>
2015-11-08 00:03:23 -0500

    ignoring

--- make.ps1
+++
@@ -1,9 +0,0 @@
-#Requires -Version 4.0
-
-$tmpl = $PSScriptRoot | Join-Path -ChildPath 'index_template.html' | Get-Item | Get-Content -Raw
-
-$PSScriptRoot | Join-Path -ChildPath 'Backends' | Get-ChildItem -Filter *.html | ForEach-Object {
-    $provider = $_.BaseName
-    $pin = $tmpl -creplace '@@CONTENT@@',(Get-Content $_.FullName -Raw)
-    $pin | Set-Content -LiteralPath ($PSScriptRoot | Join-Path -ChildPath "index_$provider.html") -Force
-}
